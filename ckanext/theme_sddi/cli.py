2154024d8c0694dc4023503a5b4de6cc23ced1f5
tino097 <konstantin.sivakov@gmail.com>
2024-06-10 18:00:27 +0200

    Implement group hierarchy with the master plugins

--- ckanext/theme_sddi/cli.py
+++ ckanext/theme_sddi/cli.py
@@ -1,19 +1,70 @@
+import os
+import json
 import click
 
+import ckan.lib.navl.dictization_functions as dict_fns
+import ckan.plugins.toolkit as tk
+import ckan.model as model
 
-@click.group(short_help="theme_sddi CLI.")
+from ckan.cli import error_shout
+
+from ckanext.theme_sddi.helpers import get_init_data
+
+
+NotFound = tk.ObjectNotFound
+NotAuthorized = tk.NotAuthorized
+ValidationError = tk.ValidationError
+
+HERE = os.path.dirname(__file__)
+_site_url = tk.config.get('ckan.site_url')
+
+
+@click.group("theme-sddi", short_help="SDDI commands")
 def theme_sddi():
-    """theme_sddi CLI.
-    """
     pass
 
 
-@theme_sddi.command()
-@click.argument("name", default="theme_sddi")
-def command(name):
-    """Docs.
-    """
-    click.echo("Hello, {name}!".format(name=name))
+def create_org_or_group(group, is_org=False):
+    user = tk.get_action('get_site_user')({'ignore_auth': True})
+    context = {
+            "model": model,
+            "session": model.Session,
+            "user": user['name'],
+            "return_id_only": True
+        }
+
+    grp_or_org = 'organization_create' if is_org else 'group_create'
+    if group.get('image_url'):
+        group['image_url'] = _site_url + group.get('image_url')
+
+    try:
+        tk.get_action(grp_or_org)(context, group)
+    except (NotFound, NotAuthorized) as e:
+        error_shout(e)
+        raise click.Abort()
+    except dict_fns.DataError:
+        error_shout('Integrity Error')
+        raise click.Abort()
+    except ValidationError as e:
+        error_shout(e)
+        raise click.Abort()
+
+
+@theme_sddi.command("init_data")
+def init_data():
+
+    data = get_init_data()
+
+    for group in data.get('groups'):
+        create_org_or_group(group)
+    for org in data.get('organizations'):
+        create_org_or_group(org, is_org=True)
+
+    click.secho(
+        "Successfully created the initial Orgs/Groups",
+        fg="green",
+        bold=True
+    )
 
 
 def get_commands():
--- ckanext/theme_sddi/plugin.py
+++ ckanext/theme_sddi/plugin.py
@@ -1,43 +1,101 @@
+import logging
+
 import ckan.plugins as plugins
-import ckan.plugins.toolkit as toolkit
+
+import ckanext.theme_sddi.cli as cli
+import ckanext.theme_sddi.logic.action as action
+import ckanext.theme_sddi.helpers as h
+import ckanext.theme_sddi.middleware as middleware
+
+tk = plugins.toolkit
+
+log = logging.getLogger(__name__)
 
 
 class ThemeSddiPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
-
-    # plugins.implements(plugins.IAuthFunctions)
-    # plugins.implements(plugins.IActions)
-    # plugins.implements(plugins.IBlueprint)
-    # plugins.implements(plugins.IClick)
-    # plugins.implements(plugins.ITemplateHelpers)
-    # plugins.implements(plugins.IValidators)
+    plugins.implements(plugins.IConfigurer, inherit=True)
+    plugins.implements(plugins.IActions, inherit=True)
+    plugins.implements(plugins.ITemplateHelpers, inherit=True)
+    plugins.implements(plugins.IPackageController, inherit=True)
+    plugins.implements(plugins.IMiddleware, inherit=True)
+    plugins.implements(plugins.IClick)
+    plugins.implements(plugins.IFacets, inherit=True)
 
     # IConfigurer
     def update_config(self, config_):
-        toolkit.add_template_directory(config_, "templates")
-        toolkit.add_public_directory(config_, "public")
-        toolkit.add_resource("assets", "theme_sddi")
+        tk.add_template_directory(config_, "templates")
+        tk.add_public_directory(config_, "public")
+        tk.add_resource("public/scripts/vendor/jstree", "jstree")
+        tk.add_resource("assets", "theme_sddi")
 
-    # IAuthFunctions
-    # def get_auth_functions(self):
-    #     return auth.get_auth_functions()
+# IActions
 
-    # IActions
-    # def get_actions(self):
-    #     return action.get_actions()
+    def get_actions(self):
+        return {
+            "group_tree_children_g": action.group_tree_children_g,
+            "user_create": action.user_create,
+            "user_update": action.user_update,
+        }
 
-    # IBlueprint
-    # def get_blueprint(self):
-    #     return views.get_blueprints()
+    def update_config_schema(self, schema):
+        ignore_missing =tk.get_validator(u'ignore_missing')
+        unicode_safe = tk.get_validator(u'unicode_safe')
+
+        schema.update({
+            u'ckan.site_intro_paragraph': [ignore_missing, unicode_safe],
+            u'ckan.background_image': [ignore_missing, unicode_safe],
+            u'image_upload': [ignore_missing, unicode_safe],
+            u'clear_image_upload': [ignore_missing, unicode_safe],
+        })
+        return schema
+        
+    # ITemplateHelpers
+
+    def get_helpers(self):
+        return {
+            "get_selected_group": h.get_selected_group,
+            "get_allowable_children_groups": h.get_allowable_children_groups,
+            "get_group_image": h.get_group_image,
+            "group_tree_crumbs": h.group_tree_crumbs,
+            "group_tree_section_g": h.group_tree_section_g,
+            "get_recently_modified_group": h.get_recently_modified_group,
+        }
 
     # IClick
-    # def get_commands(self):
-    #     return cli.get_commands()
 
-    # ITemplateHelpers
-    # def get_helpers(self):
-    #     return helpers.get_helpers()
+    def get_commands(self):
+        return cli.get_commands()
 
-    # IValidators
-    # def get_validators(self):
-    #     return validators.get_validators()
+    # IMiddleware
+
+    def make_middleware(self, app, config):
+        app.before_request(middleware.ckanext_before_request)
+        return app
+
+    # IFacets
+
+    def dataset_facets(self, facets_dict, package_type):
+        # Get Main and Topics group
+        del facets_dict['groups']
+        facets_dict['main'] = tk._('Main Categories')
+        facets_dict['topics'] = tk._('Topics')
+
+        return facets_dict
+
+    # IPackageController
+
+    def before_index(self, pkg_dict):
+        # Get the group hierarchy
+        groups = pkg_dict.get("groups", [])
+        if not groups:
+            return pkg_dict
+        for group in groups:
+            group_dict = tk.get_action("group_show")({}, {"id": group})
+            groups_data = group_dict.get("groups", [])
+            if groups_data[0]["name"] == "main-categories":
+                pkg_dict["main"] = group
+            else:
+                pkg_dict["topics"] = group
+
+        return pkg_dict
